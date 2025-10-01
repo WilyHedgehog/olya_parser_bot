2f43e6d6993f588ada76e07809f296658a3acf78
Oleg Ezhikov <egikww@gmail.com>
2025-10-01 06:03:34 +0300

    add_chat_admin_func5.3

--- bot/handlers/chat_admin.py
+++ bot/handlers/chat_admin.py
@@ -1,7 +1,7 @@
 from aiogram import F
 from aiogram.types import ChatMemberUpdated
 from bot_setup import bot
-from aiogram.types import ChatMemberStatus
+from aiogram.types.chat_member import ChatMemberStatus
 import asyncio
 from aiogram import Router
 
