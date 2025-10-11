525f8fb5279a3c32f1c1a328bb77353d307fac3a
Oleg Ezhikov <egikww@gmail.com>
2025-10-11 16:29:30 +0300

    taskiq_41

--- bot/background_tasks/aps_utils.py
+++ bot/background_tasks/aps_utils.py
@@ -4,4 +4,4 @@ from bot.background_tasks.broker import schedule_source
 async def clear():
     schedules = await schedule_source.get_schedules()
     for s in schedules:
-        await schedule_source.delete_schedule(s)
+        await schedule_source.delete_schedule(s.schedule_id)
