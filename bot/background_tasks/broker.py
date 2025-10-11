370f9f36c929f1dfe372beb7a06a45af97268841
Oleg Ezhikov <egikww@gmail.com>
2025-10-11 12:57:00 +0300

    taskiq_29

--- bot/background_tasks/broker.py
+++ bot/background_tasks/broker.py
@@ -1,11 +1,11 @@
 # bot/background_tasks/broker.py
-from taskiq_nats import PullBasedJetStreamBroker, PushBasedJetStreamBroker
+from taskiq_nats import PullBasedJetStreamBroker
 from taskiq_nats.schedule_source import NATSKeyValueScheduleSource
 from config.config import load_config
 
 config = load_config()
 
-broker = PushBasedJetStreamBroker(
+broker = PullBasedJetStreamBroker(
     servers=config.nats.servers,
     queue="taskiq_broadcasts",
 )
